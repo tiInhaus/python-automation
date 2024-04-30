b8c1f7eb2a28911d071a16cd644ef5562ef91d67
Murillo Vicente <muuh.renai@gmail.com>
2024-04-30 15:05:31 +0000

    executaveis

--- EXECUTAVEIS/FLASK.bat
+++ EXECUTAVEIS/FLASK.bat
@@ -1,1 +1,1 @@
-"C:\Users\app_rasp\AppData\Local\Programs\Python\Python39\python.exe" "C:\PYTHON_JOBS\FLASK\run.py"
+"C:\Users\app_rasp\AppData\Local\Programs\Python\Python39\python.exe" "C:\PYTHON_JOBS\python-automation\FLASK\run.py"
